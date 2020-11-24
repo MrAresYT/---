cac04ac5026b99d93bb66ca61e74b883362dbeb9
Mr_Ares <dime.egorkin.2015@mail.ru>
2020-11-24 12:55:33 +0300

    Зачем множить безполезные репозитории?

---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from random import randint
+
+
+def get_random_size():
+    size = randint(50, 300)
+    return randint(0, 500), randint(0, 500), size, size
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Жёлтые окружности')
+        self.setFixedSize(600, 600)
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.setFixedSize(100, 50)
+        self.btn.move(0, 0)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+        self.do_paint = False
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        # Рисуем прямоугольник заданной кистью
+        qp.drawEllipse(*get_random_size())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
